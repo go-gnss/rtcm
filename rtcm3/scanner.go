548e7de0dcc5ce641a522df494a8197f2d7d05de
Brandon <brandon.owen@hotmail.com>
2020-04-26 18:47:39 +1000

    Rename rtcm3.go to message.go

--- rtcm3/scanner.go
+++ rtcm3/scanner.go
@@ -14,7 +14,7 @@ func NewScanner(r io.Reader) Scanner {
 	return Scanner{bufio.NewReader(r)}
 }
 
-// Next reads from IO until a Message is found
+// Next reads from Scanner until a Message is found
 func (scanner Scanner) Next() (message Message, err error) {
 	frame, err := scanner.NextFrame()
 	if err != nil {
@@ -23,6 +23,7 @@ func (scanner Scanner) Next() (message Message, err error) {
 	return DeserializeMessage(frame.Payload), err // probably have DeserializeMessage return err
 }
 
+// NextFrame reads from Scanner until a valid Frame is found
 func (scanner Scanner) NextFrame() (frame Frame, err error) {
 	for {
 		frame, err := DeserializeFrame(scanner.Reader)
