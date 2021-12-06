d6668126fe861c698ee927506abcb5a826635096
Brandon <brandon.owen@hotmail.com>
2021-12-06 18:59:49 +1100

    Improve ntriplatency script

--- cmd/ntriplatency/latency.go
+++ cmd/ntriplatency/latency.go
@@ -1,33 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/go-gnss/rtcm/rtcm3"
-	"github.com/go-gnss/ntrip"
+	"net/http"
+	"os"
 	"time"
+
+	"github.com/go-gnss/ntrip"
+	"github.com/go-gnss/rtcm/rtcm3"
 )
 
 func main() {
-	caster := flag.String("caster", "http://auscors.ga.gov.au:2101/ALIC7", "NTRIP caster mountpoint to stream from")
+	caster := flag.String("caster", "https://ntrip.data.gnss.ga.gov.au/ALIC00AUS0", "NTRIP caster address, must include scheme")
 	username := flag.String("username", "", "NTRIP username")
 	password := flag.String("password", "", "NTRIP password")
+	outputJson := flag.Bool("json", false, "output RTCM data as JSON objects instead of printing message latency")
 	flag.Parse()
 
-	client, err := ntrip.NewClient(*caster)
-	client.SetBasicAuth(*username, *password)
-	resp, err := client.Connect()
-	if err != nil || resp.StatusCode != 200 {
-		fmt.Println(resp.StatusCode, err)
+	req, _ := ntrip.NewClientRequest(*caster)
+	req.SetBasicAuth(*username, *password)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("request failed with error: %s\n", err)
+		os.Exit(1)
+	}
+	if resp.StatusCode != 200 {
+		fmt.Printf("request failed with response code %d\n", resp.StatusCode)
+		os.Exit(1)
 	}
 
 	scanner := rtcm3.NewScanner(resp.Body)
 	for msg, err := scanner.NextMessage(); err == nil; msg, err = scanner.NextMessage() {
+		if *outputJson {
+			out, _ := json.Marshal(msg)
+			fmt.Println(string(out))
+			// Don't output latency if outputting json (so stdout is parsable by jq)
+			continue
+		}
+
 		if obs, ok := msg.(rtcm3.Observation); ok {
 			fmt.Println(msg.Number(), time.Now().UTC().Sub(obs.Time()))
 		} else {
-			fmt.Println(msg.Number())
+			fmt.Println(msg.Number(), "N/A")
 		}
-		fmt.Printf("%+v\n\n", msg)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,8 @@ go 1.14
 
 require (
 	github.com/bamiaux/iobit v0.0.0-20170418073505-498159a04883
-	github.com/go-gnss/ntrip v0.0.0-20200406053254-a4467bfee793
+	github.com/go-gnss/ntrip v0.0.8
 	github.com/go-restruct/restruct v1.2.0-alpha.0.20210525045353-983b86fa188e
 	github.com/google/go-cmp v0.3.1
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.5.1 // indirect
 )
